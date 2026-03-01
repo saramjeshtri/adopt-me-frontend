22ac3168f246fb649b55d3bdfe12a38b4b31a9f3
sara <saramjeshtri12@gmail.com>
2026-03-01 15:17:13 +0100

    chore: add routes for education, contact, surrender, events

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,17 @@ import AnimalsPage from './pages/public/AnimalsPage'
 import AnimalDetailPage from './pages/public/AnimalDetailPage'
 import ReportPage from './pages/public/ReportPage'
 import TrackReportPage from './pages/public/TrackReportPage'
+import EducationPage from './pages/public/EducationPage'
+import ArticlePage from './pages/public/ArticlePage'
+import ContactPage from './pages/public/ContactPage'
+
 import AdminLayout from './pages/admin/AdminLayout'
 import AdminReportsPage from './pages/admin/AdminReportsPage'
 import AdminAnimalsPage from './pages/admin/AdminAnimalsPage'
 import AdminMeetingsPage from './pages/admin/AdminMeetingsPage'
+import AdminEventsPage from './pages/admin/AdminEventsPage'
+import AdminSurrenderPage from './pages/admin/AdminSurrenderPage'
+
 
 function PublicLayout({ children }: { children: React.ReactNode }) {
   return (
@@ -30,10 +37,15 @@ export default function App() {
       <Route path="/adopto/:id" element={<PublicLayout><AnimalDetailPage /></PublicLayout>} />
       <Route path="/raporto" element={<PublicLayout><ReportPage /></PublicLayout>} />
       <Route path="/track" element={<PublicLayout><TrackReportPage /></PublicLayout>} />
+      <Route path="/edukim" element={<PublicLayout><EducationPage /></PublicLayout>} />
+      <Route path="/edukim/artikull/:id" element={<PublicLayout><ArticlePage /></PublicLayout>} />
+      <Route path="/kontakt" element={<PublicLayout><ContactPage /></PublicLayout>} />
       <Route path="/admin" element={<AdminLayout />}>
-        <Route path="reports" element={<AdminReportsPage />} />
-        <Route path="animals" element={<AdminAnimalsPage />} />
-        <Route path="meetings" element={<AdminMeetingsPage />} />
+        <Route path="reports"   element={<AdminReportsPage />} />
+        <Route path="animals"   element={<AdminAnimalsPage />} />
+        <Route path="meetings"  element={<AdminMeetingsPage />} />
+        <Route path="events"    element={<AdminEventsPage />} />
+        <Route path="surrender" element={<AdminSurrenderPage />} />
       </Route>
     </Routes>
   )
